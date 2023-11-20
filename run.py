22e61d0abdf3b923d61ff87c6a57c09b33d45047
queziaa <queziaa31@gmail.com>
2023-11-20 19:02:55 +0800

    2.0

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
+import socket
 import time
 import sys
-# from run_spider import ma
 import os
 import datetime
 import subprocess
@@ -9,6 +9,7 @@ import string
 import random
 
 
+dir = '/mnt/hgfs/Distributed/'
 encode='UTF-8'
 netId = ['32309233','225310',
 '32309168','13661X',
@@ -41,9 +42,24 @@ netId = ['32309233','225310',
 '32309265','085138',
 '32309169','237318']
 
-def find_files_starting_with_zero(directory):
-    return [f for f in os.listdir(directory) if f.startswith('0')]
-
+def find_files_starting(directory,n,ip=False):
+    out = []
+    if ip:
+        for f in os.listdir(directory):
+            if ip in f:
+                if f.find('_') == -1:
+                    continue
+                ff = int(f.split('_')[0])
+                if ff != n:
+                    out.append(f)
+    else:
+        for f in os.listdir(directory):
+            if f.find('_') == -1:
+                continue
+            ff = int(f.split('_')[0])
+            if ff == n:
+                out.append(f)
+    return out
 def read_and_delete_first_line(filename):
     with open(filename, 'r',encoding=encode) as file:
         lines = file.readlines()
@@ -94,7 +110,7 @@ def be(mode,keywords,start_time,end_time,proxy,cookie):
     codaHttps = 0
     text = '''def set():
     return{'''
-    filename = "temp/set.py"
+    filename = "./temp/set.py"
     now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
     text = text +  ''''mode':'{}','keywords':'{}','end_time':'{}','start_time':'{}','now':'{}','proxy':'{}','cookie':'{}' '''.format(mode,keywords,end_time,start_time,now,proxy,cookie) + '}'
     if os.path.exists(filename):
@@ -104,7 +120,7 @@ def be(mode,keywords,start_time,end_time,proxy,cookie):
     with open(filename, mode,encoding=encode) as f:
         f.write(text)
     
-    filename="../output/{}.txt".format(now)
+    filename= dir + "output/{}.txt".format(now)
     text = mode+" "+keywords+" "+start_time+" "+end_time+" "+now+" "+proxy+" "+cookie
     if os.path.exists(filename):
         mode = 'w'  # set mode to write to existing file
@@ -113,14 +129,14 @@ def be(mode,keywords,start_time,end_time,proxy,cookie):
     with open(filename, mode,encoding=encode) as f:
         f.write(text)
     # 定义要运行的命令
-    command = ['python', 'run_spider.py']
-    filename="../output/{}.log".format(now)
+    command = ['python3', 'run_spider.py']
+    filename= dir + "output/{}.log".format(now)
     # 打开一个文件来保存输出
     with open(filename, 'w',encoding=encode) as f:
         # 运行命令并将输出写入文件
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        encode2 = 'gbk'
-        # encode2 = 'UTF-8'
+        # encode2 = 'gbk'
+        encode2 = 'UTF-8'
         for line in iter(process.stdout.readline, b''):
             de = line.decode(encode2)
             f.write(de)
@@ -133,12 +149,25 @@ def be(mode,keywords,start_time,end_time,proxy,cookie):
     process.wait()
     return [code414,codaHttps]
 
+def ping_website(website):
+    try:
+        output = subprocess.check_output("ping -c 1 " + website, shell=True)
+        output = output.decode("utf-8")
 
-def connected():
-    response = os.popen("ping -n 1 weibo.com").read()
-    if response != 0:
+        if "1 packets transmitted, 1 received" in output:
+            return True
+        else:
+            return False
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return False
+    
+def connected(ID):
+    response = ping_website("weibo.com")
+    if response == False:
         random_int = random.randint(0, 29)  # 生成0至30的随机整数
-        filename = "a.json"
+        print("尝试登录校园网",ID)
+        filename = "./temp/" + ID + ".json"
         text = '''{"server":"http://172.31.99.50","strict_bind":false,"double_stack":false,"retry_delay":300,"retry_times":1,"n":200,"type":1,"acid":4,"os":"Windows","name":"Windows98","users":[{"username":"'''
         text = text + '''{}","password":"{}","ip":"{}'''.format(netId[random_int*2],netId[random_int*2+1],socket.gethostbyname(socket.gethostname()))
         text = text + '''"}]}'''
@@ -148,47 +177,67 @@ def connected():
             mode = 'x'  # set mode to create new file
         with open(filename, mode, encoding=encode) as f:
             f.write(text)
-        response = os.popen("sdusrun.exe login -c a.json").read()
-        for line in response.splitlines():
-            if 'online_ip' in line:
-                line = line.replace('online_ip', '')
-                line = line.replace(' ', '')
-                line = line.replace('\n', '')
-                line = line.replace(',', '')
-                line = line.replace('"', '')
-                return line
+        response = os.popen("./sdusrun1 login -c " + filename).read()
+        return response
 
-import socket
-
+def get_data_from_api():
+    try:
+        response = requests.get("https://www.ipplus360.com/getIP")
+        data = response.json()
+        return data['data']
+    except Exception as e:
+        print("获取IP失败: ",e)
+        return None
 if __name__ == '__main__':
     index = int(sys.argv[1])
     cookieLose = False
+    ipccupy = False
     workNull = False
     code414 = False
     ip = ''
-    dir = 'Z:\\Distributed\\'
     co = read_specific_line(dir + "cookies.txt", index)
-    ID = ''.join(random.choice(string.ascii_letters) for _ in range(10))
-    ID = '0_' + str(index) + '_' + ID
+    ID = str(index)
     print('**********************************')
     while True:
         time.sleep(1)
-        ip = connected()
+        connected(ID)
+        ip = get_data_from_api()
+        if ip == None:
+            continue
+        temp = find_files_starting(dir,index)
+        if temp != []:
+            if len(temp) > 1:
+                print('错误: ID:' + ID + ' 有多个IP文件')
+                exit()
+            temp = temp[0]
+            os.remove(dir + temp)         
+        with open(dir + ID + "_" + ip, 'w') as f:
+                pass
+        temp = find_files_starting(dir,index,ip=ip)
+        if temp != []:
+            if not ipccupy:
+                print('IP ',ip,' 已被占用')
+                ipccupy = True
+            continue
+        else:
+            ipccupy = False
+
         if not read_cookies(co[0],co[1]):
             if not cookieLose:
-                print('cookie失效')
+                print('cookie失效 10s后重试')
+                time.sleep(10)
                 cookieLose = True
             co = read_specific_line(dir + "cookies.txt", index)
             continue
         else:
             cookieLose = False
         first_line = ''
-        if find_files_starting_with_zero(dir) == []:
+        if find_files_starting(dir,0) == []:
             if not code414:
-                with open(dir + ID, 'w') as f:
+                with open(dir + '0_' + ID, 'w') as f:
                     pass
                 first_line = read_and_delete_first_line(dir + 'works.txt')  # 读取并删除第一行        
-                os.remove(dir + ID)
+                os.remove(dir + '0_' + ID)
                 if first_line == False:
                     if not workNull:
                         print('无任务')
@@ -196,15 +245,15 @@ if __name__ == '__main__':
                     continue
                 else:
                     workNull = False
-                print('ID: ', ID + ' IP: ' + ip + ' 任务: ', first_line)
+                print('ID:', ID + ' IP: ' + ip + ' 任务: ', first_line)
 
             recode = be('tweet_by_user_id',first_line,'0','0','0',co[1])
             if recode[0] >= 4 and recode[1] == 2:
                 if not code414:
-                    print('414错误')
+                    print('414错误 63s后重试')
                     code414 = True
                 time.sleep(63)
             else:
                 code414 = False
-
-    
+        else:
+            print('有未完成任务')
